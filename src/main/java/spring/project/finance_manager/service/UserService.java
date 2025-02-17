d06a18dccc22855010006df9ec2b8936535b98a9
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-02-17 22:47:55 +0200

    Created a User entity, controller, repo and service

---
+++ src/main/java/spring/project/finance_manager/service/UserService.java
@@ -0,0 +1,59 @@
+package spring.project.finance_manager.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import spring.project.finance_manager.component.AuthResponse;
+import spring.project.finance_manager.component.JwtUtil;
+import spring.project.finance_manager.entity.User;
+import spring.project.finance_manager.repository.UserRepository;
+import spring.project.finance_manager.request.LoginRequest;
+import spring.project.finance_manager.request.RegisterRequest;
+
+import java.util.ArrayList;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public UserService(UserRepository userRepository, JwtUtil jwtUtil) {
+        this.userRepository = userRepository;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public ResponseEntity<?> registerUser(RegisterRequest request) {
+        if (userRepository.findByEmail(request.getEmail()).isPresent())
+            return ResponseEntity.badRequest().body("Email is already in use!");
+
+        if (!request.getPassword().equals(request.getConfirmPassword()))
+            return ResponseEntity.badRequest().body("Passwords do not match!");
+
+        User user = new User(
+                request.getName(),
+                request.getEmail(),
+                passwordEncoder.encode(request.getPassword()),
+                new ArrayList<>()
+        );
+        userRepository.save(user);
+
+        return ResponseEntity.ok("User registered successfully!");
+    }
+
+    public ResponseEntity<?> loginUser(LoginRequest request) {
+        if (!userRepository.findByEmail(request.getEmail()).isPresent())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User with this email does not exist!");
+
+        User user = userRepository.findByEmail(request.getEmail()).get();
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword()))
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Incorrect password!");
+
+        String token = jwtUtil.generateToken(user.getEmail());
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+}
