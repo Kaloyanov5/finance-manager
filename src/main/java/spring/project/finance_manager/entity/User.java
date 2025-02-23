f01b2ef3783afe519c41331bfb8317c0dab362ee
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-02-23 22:29:10 +0200

    Added `@OneToMany` relation to new `Task` entity

--- src/main/java/spring/project/finance_manager/entity/User.java
+++ src/main/java/spring/project/finance_manager/entity/User.java
@@ -22,6 +22,9 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Transaction> transactions;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Task> tasks;
+
     public User() { }
 
     public User(Long user_id, String name, String email, String password, List<Transaction> transactions) {
