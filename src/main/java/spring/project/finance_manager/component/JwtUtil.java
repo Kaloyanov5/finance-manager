106538b570970ec9e2c69d695ee81055fd537e30
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-02-26 22:51:50 +0200

    Updated `JwtUtil.java` to create refresh tokens as well

--- src/main/java/spring/project/finance_manager/component/JwtUtil.java
+++ src/main/java/spring/project/finance_manager/component/JwtUtil.java
@@ -1,12 +1,16 @@
 package spring.project.finance_manager.component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 import static io.jsonwebtoken.Jwts.builder;
 
@@ -15,26 +19,50 @@ public class JwtUtil {
 
     @Value("${secret.key}")
     private String SECRET_KEY;
-    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 10;
 
-    public String generateToken(String email) {
+    private static final long ACCESS_TOKEN_EXPIRATION = 900000 ; // 15 minutes
+    private static final long REFRESH_TOKEN_EXPIRATION = 1209600000; // 14 days
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public String generateAccessToken(String email) {
+        return generateToken(email, ACCESS_TOKEN_EXPIRATION);
+    }
+
+    public String generateRefreshToken(String email) {
+        return generateToken(email, REFRESH_TOKEN_EXPIRATION);
+    }
+
+    public String generateToken(String email, long expiration) {
         return builder()
                 .setSubject(email)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public void validateToken(String token) throws JwtException {
-        Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
     }
 
-    public String extractEmail(String token) throws JwtException {
-        return Jwts.parser()
-                .setSigningKey(SECRET_KEY)
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
                 .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
+                .getBody();
+        return claimsResolver.apply(claims);
     }
 }
